e28dd1884cdcee5c262dc0429f7d20e0483227a0
BayarAli2002 <alibayar893@gmail.com>
2024-04-01 22:46:12 +0300

    Switch Statement

--- ConditionStatement.dart
+++ ConditionStatement.dart
@@ -7,7 +7,7 @@ main() {
     print("$b is greater than $a");
   }
 
-//OPerator Condition
+//OPerator Condition or Short if
 
   a > b ? print("$a is greater than $b") : print("$b is greater than $a");
   var temp = a > b ? a : b;
@@ -15,8 +15,11 @@ main() {
 
 // check if the variable is null or not
 
+//we can not store <null> in (string) datatype
   var name = null;
   var str = name ?? "Bayar";
+// it will print "Bayar" otherwise it will print the value assigned to the varibale name
+//(??) checking if the value is null or not .
 
   print(str);
 }
---
+++ Switch.dart
@@ -0,0 +1,23 @@
+main() {
+  var num = 20;
+  switch (num) {
+    case 100:
+      print("Excellent");
+    case 90:
+      print("Excellent");
+      break;
+    case 80:
+      print("Very good");
+      break;
+    case 70:
+      print("Good");
+      break;
+    case 60:
+      print("Medium");
+    case 50:
+      print("satisfied");
+      break;
+    default:
+      print("failed");
+  }
+}
