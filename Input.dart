d0adfe361fc056bc469d2ad2b5f26ea080a1c5b5
BayarAli2002 <alibayar893@gmail.com>
2024-04-06 18:03:47 +0300

    input

--- Input.dart
+++ Input.dart
@@ -1,26 +1,15 @@
 import 'dart:io';
 
+//this Library for input and output
 void main() {
   print("Enter your Birth Year:");
-
-  // Read input
-  var birthYear = stdin.readLineSync();
-
-  // when we enter an input we musr Check if the input is not null
-  if (birthYear != null) {
-    // Parse the birth year as an integer
-    var birthYearInt = num.parse(birthYear);
-
-    // Check if parsing was successful
-    if (birthYearInt != null) {
-      // Calculate the age
-      var age = DateTime.now().year - birthYearInt;
-
-      print("Your age is $age years old");
-    } else {
-      print("Invalid input. Please enter a valid year.");
-    }
+  //Read and input
+  var birth_years = stdin.readLineSync();
+//when we enter an input we must check if its null or not
+  if (birth_years != null) {
+    var age = DateTime.now().year - num.parse(birth_years);
+    print("your birth year is $age ");
   } else {
-    print("No input provided.");
+    print("Error:enter a birth year");
   }
 }
